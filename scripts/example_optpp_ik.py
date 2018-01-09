f30ba165cf8309cf5ce9812b64a33e522f94b1bb
Wolfgang Merkt <w.merkt@gmail.com>
2018-01-09 17:49:23 +0000

    Fix test after is_shutdown has been deprecated, clean un-load on exit

--- scripts/example_optpp_ik.py
+++ scripts/example_optpp_ik.py
@@ -7,24 +7,38 @@ import math
 from pyexotica.publish_trajectory import *
 from time import sleep
 
+
 def figureEight(t):
     return array([0.6, -0.1 + math.sin(t * 2.0 * math.pi * 0.5) * 0.1, 0.5 + math.sin(t * math.pi * 0.5) * 0.2, 0, 0, 0])
 
 exo.Setup.initRos()
-(sol, prob)=exo.Initializers.loadXMLFull('{optpp_solver}/resources/optpp_ik.xml')
+(sol, prob) = exo.Initializers.loadXMLFull(
+    '{optpp_solver}/resources/optpp_ik.xml')
 problem = exo.Setup.createProblem(prob)
 solver = exo.Setup.createSolver(sol)
 solver.specifyProblem(problem)
 
-dt=0.002
-t=0.0
-q=array([0.0]*7)
-print('Publishing IK')
-while not is_shutdown():
-    problem.setGoal('Position',figureEight(t))
-    problem.startState = q
-    q = solver.solve()[0]
-    publishPose(q, problem)    
-    sleep(dt)
-    t=t+dt
+import signal
 
+
+def sigIntHandler(signal, frame):
+    raise KeyboardInterrupt
+signal.signal(signal.SIGINT, sigIntHandler)
+
+dt = 0.002
+t = 0.0
+q = array([0.0]*7)
+print('Publishing IK')
+while True:
+    try:
+        problem.setGoal('Position', figureEight(t))
+        problem.startState = q
+        q = solver.solve()[0]
+        publishPose(q, problem)
+        sleep(dt)
+        t = t+dt
+    except KeyboardInterrupt:
+        break
+
+# Avoid heap issues
+del problem, solver
